d8bc4c15c6a4abfade25a510fe80b60af85efa13
Methilan-R <rmethilan@gmail.com>
2025-05-27 18:11:42 +0530

    asset

--- app/components/Navbar/MobileNav.tsx
+++ app/components/Navbar/MobileNav.tsx
@@ -3,15 +3,19 @@ import Link from 'next/link'
 import React from 'react'
 import { CgClose } from 'react-icons/cg'
 
-const MobileNav = () => {
+type Props = {
+    showNav: boolean;
+    closeNav: () => void;
+}
+const MobileNav = ({closeNav,showNav}:Props) => {
+    const navOpen = showNav ? "translate-x-0":"translate-x-[-100%]"
   return (
     <div >
        {/*overlay*/}
-       <div className="fixed insert-0 transform transition-all duration-500 z-{1002} bg-black opacity-70 w-full
-       h-screen">
+       <div className={`fixed ${navOpen} insert-0 transform transition-all duration-500 z-{1002} bg-black opacity-70 w-full h-screen`}></div>
         {/* navlink */}
-        <div className='text-white fixed justify-center flex flex-col h-full transform transition-all duration-500
-        delay-300 w-[80%] sm:w-[60%] bg-rose-900 space-y-6 z-[1050]'>
+        <div className={`text-white ${navOpen} fixed justify-center flex flex-col h-full transform transition-all duration-500
+        delay-300 w-[80%] sm:w-[60%] bg-rose-900 space-y-6 z-[1050]`}>
             {navLinks.map((link)=>{
                 return(
                     <Link key={link.id} href={link.url}>
@@ -21,9 +25,10 @@ const MobileNav = () => {
                     </Link>
                 )
             })}
-        </div>
-        {/*close button */}
-        <CgClose className="text-white w-8 h-8 hover:text-yellow-400 transition duration-300" />
+            {/*close button */}
+        <CgClose
+        onClick={closeNav}
+        className="absolute top-8 right-[1.4rem] w-8 h-8 sm:w-8 sm:h-6" />  
        </div>
     </div>
   )
--- app/components/Navbar/Nav.tsx
+++ app/components/Navbar/Nav.tsx
@@ -1,13 +1,29 @@
+"use client"
 import { navLinks } from '@/app/constant/constant'
 import { link } from 'fs'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { HiBars3BottomRight } from 'react-icons/hi2'
 import { TbAirBalloon } from 'react-icons/tb'
 
-const Nav = () => {
+type Props ={
+    openNav:()=>void
+}
+const Nav = ({openNav}:Props) => {
+    const [navBg, setNavBg] = useState(false);
+
+    useEffect(()=>{
+        const handler = () => {
+            if(window.scrollY >=90) setNavBg(true);
+            if(window.scrollY <90) setNavBg(false);
+        };
+        window.addEventListener("scroll",handler);
+        return () => window.removeEventListener("scroll",handler);
+    },[]);
   return (
-    <div className='bg-blue-950 transition-all duration-200 h-[12vh] z-[1000] fixed w-full '>
+    <div className={`bg-blue-950 ${
+        navBg ? " bg-blue-950 shadow-md" : "fixed"
+    } transition-all duration-200 h-[12vh] z-[1000] fixed w-full`}>
       <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
         {/*logo*/}
         <div className='flex items-center space-x-2'>
@@ -40,7 +56,9 @@ const Nav = () => {
                 Book Now
             </button>
             {/*Burger Menu*/}
-            <HiBars3BottomRight className='w-8 h-8 cursor-pointer text-white lg:hidden'/>
+            <HiBars3BottomRight
+            onClick={openNav}
+            className='w-8 h-8 cursor-pointer text-white lg:hidden'/>
          </div>
       </div>
     </div>
