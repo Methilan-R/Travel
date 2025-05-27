3bbecaae338cdb2fef5c996aeea342d9558e3202
Methilan-R <rmethilan@gmail.com>
2025-05-27 10:51:50 +0530

    h

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function HomePage() {
   return (
      <h1 className="text-3xl font-bold underline">
       Home page
