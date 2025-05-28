4c150133185bcd8f36fbc7071468beba7233247f
Methilan-R <rmethilan@gmail.com>
2025-05-28 06:15:16 +0530

    asset

--- app/components/Home/Hero/Hero.tsx
+++ app/components/Home/Hero/Hero.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
-
+ 
 const Hero = () => {
   return (
     <div className='relative w-full h-[120vh]'>
         {/*Overlay */}
       <div className='absolute top-0 left-0 w-full h-full bg-gray-800 opacity-70'></div>
-      <video src="/asset/hero1.mp4" autoPlay muted loop preload='metadata'
+      <video src="/asset/hero1.mp4"
+      autoPlay 
+      muted 
+      loop preload='metadata'
       className='w-full h-full object-cover'/>
+      {/*Text Content */}
+      <div className="absolute z-[100]"></div>
     </div>
   )
 }
