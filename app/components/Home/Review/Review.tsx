81dd8669130bd1a6500cddd7411b71789dd5b67a
Methilan-R <rmethilan@gmail.com>
2025-05-28 13:44:14 +0530

    news

--- app/components/Home/Review/Review.tsx
+++ app/components/Home/Review/Review.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FaStar } from 'react-icons/fa'
+import ReviewSlider from './ReviewSlider'
 
 const Review = () => {
   return (
@@ -9,9 +11,32 @@ const Review = () => {
                 <h1 className='text-2xl font-semibold text-white'>
                   What our customers are saying us?
                 </h1>
+                <p className="mt-6 text-gray-200">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                   Exercitationem ducimus expedita, nesciunt fugit iure similique blanditiis
+                    consectetur tempora odio dolor eos unde commodi iste quo veniam vitae tempore
+                     deserunt harum?
+                </p>
+                {/*Rating */}
+                <div className='mt-6 flex items-center spce-x-6'>
+                <div>
+                  <p className="text-2xl font-bold text-white">4.88</p>
+                  <p className="text-white mb-2">Overall Rating</p>
+                  <div className="flex items-center">
+                    <FaStar className='text-white'/>
+                    <FaStar className='text-white'/>
+                    <FaStar className='text-white'/>
+                    <FaStar className='text-white'/>
+                    <FaStar className='text-white'/>
+                  </div>
+                </div>
+                </div>
+            </div>
+            {/*Slider */}
+            <div className="overflow-hidden">
+              <ReviewSlider/>
             </div>
         </div>
-      
     </div>
   )
 }
