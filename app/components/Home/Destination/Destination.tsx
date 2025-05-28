218d7c77cab307638f4c975b1c793a42445fd2a9
Methilan-R <rmethilan@gmail.com>
2025-05-28 07:50:28 +0530

    des

---
+++ app/components/Home/Destination/Destination.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SectionHeading from '../../Helper/SectionHeading'
+
+const Destination = () => {
+  return (
+    <div className='pt-20 pb-20'>
+        {/* section heading */}
+        <SectionHeading heading='Exploring Popular Destination'/>
+        {/** */}
+        <div>
+
+        </div> 
+    </div>
+  )
+}
+
+export default Destination
